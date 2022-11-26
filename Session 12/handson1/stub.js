cecb2b6adf552e3a7d42c5ab9a7ee6349e3157c1
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-11-26 10:46:39 +0530

    Update stub.js

--- Session 12/handson1/stub.js
+++ Session 12/handson1/stub.js
@@ -2,3 +2,6 @@
 TODO: Write a setTimeout() method in ES6 syntax which is executed after 5 seconds and prints the message 
 "Welcome to upGrad" on the console.
 */
+setTimeout(()=>{
+  console.log("Welcome to upGrad");
+},5000)
